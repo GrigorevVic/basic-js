ac8b297a55a788ba3d63f5611709a0976a439915
Grigorev Viktor <4342718@gmail.com>
2023-12-20 21:56:12 +0300

    feat: implemented season + repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let { repeatTimes, separator = '+', addition = '', additionRepeatTimes, additionSeparator = '|'} = options;
+  str = String(str);
+  addition = String(addition);
+  if (!repeatTimes) return `${str}${addition}`;
+  let addSep = '';
+  if (additionRepeatTimes) {
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      addSep += `${addition}`;
+      if(i === additionRepeatTimes - 1) break;
+      addSep += `${additionSeparator}`;
+    }
+
+    addition = addSep.length > addition.length ? addSep : addition;
+  }
+  let res = '';
+  for (let i = 0; i < repeatTimes; i++) {
+    res += `${str}${addition}`;
+    if(i === repeatTimes - 1) break;
+    res += `${separator}`;
+  }
+  return res;
 }
 
 module.exports = {
