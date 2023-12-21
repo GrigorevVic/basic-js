61374c1972c7090735b5cf0d45757b0246a3ad77
Grigorev Viktor <4342718@gmail.com>
2023-12-21 21:50:07 +0300

    feat: add vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -10,24 +10,79 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * const reverseMachine = new VigenereCipheringMachine(false);
  * 
- * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
  * 
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
  * 
- * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
- * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+const alphabet = [
+  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(type = true) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    let str = '';
+    let indexKey;
+    let indexMessage;
+    let offset;
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (j === key.length) j = 0;
+      indexKey = alphabet.indexOf(key[j].toLowerCase());
+      indexMessage = alphabet.indexOf(message[i].toLowerCase());
+      offset = indexMessage + indexKey;
+
+      if (!alphabet.includes(message[i].toLowerCase())) {
+        str += message[i];
+        continue;
+      }
+
+      if (offset <= 25) {
+        str += alphabet[offset].toUpperCase();
+        j++;
+      } else {
+        str += alphabet[offset - 26].toUpperCase();
+        j++;
+      }
+    }
+    return str;
   }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    let str = '';
+    let indexKey;
+    let indexMessage;
+    let offset;
+    for (let i = 0, j = 0; i < message.length; i++) {
+        if (j === key.length) j = 0;
+        indexKey = alphabet.indexOf(key[j].toLowerCase());
+        indexMessage = alphabet.indexOf(message[i].toLowerCase());
+        offset = indexMessage - indexKey;
+        if (offset < 0) offset = 26 + (indexMessage - indexKey);
+  
+        if (!alphabet.includes(message[i].toLowerCase())) {
+          str += message[i];
+          continue;
+        }
+  
+        if (offset <= 25) {
+        str += alphabet[offset].toUpperCase();
+        j++;
+        } else {
+        str += alphabet[offset - 26].toUpperCase();
+        j++;
+        }
+    }
+  return str;
+   }
 }
 
 module.exports = {
